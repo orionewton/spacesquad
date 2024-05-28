f2dab87f8c6f3e607159ec5b5b09d8016d6b2b5a
LTytgat <orionewton@hotmail.fr>
2024-05-28 14:45:50 +0200

    Mise en place Tests

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+# tests/test_app.py
+import pytest
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_index(client):
+    """Test que la page d'accueil se charge correctement."""
+    rv = client.get('/')
+    assert rv.status_code == 200
