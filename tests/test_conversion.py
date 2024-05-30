e4f979e7834b68152e2a756a5822e479f2338088
LTytgat <orionewton@hotmail.fr>
2024-05-30 02:34:40 +0200

    another try

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,10 +1,20 @@
 import os
 
+import pytest
+
 from convert import Content
 
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'doc_tests',
+)
 
+
+@pytest.mark.datafiles(
+    os.path.join(FIXTURE_DIR, 'Modele.txt'),
+)
 def test_read_file():
-    content = Content("Modele.txt")
+    content = Content(os.path.join(FIXTURE_DIR, "Modele.txt"))
     assert content.author == "Auteur"
     assert content.title == "Titre"
     assert content.intro == "Introduction"
